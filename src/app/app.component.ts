d4fc9db9d3cb8e076d4f71e194b188fd2268777b
Jhoan Rojas <jhosebro2108@gmail.com>
2023-01-24 21:34:27 -0500

    Finalizacion de video 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { observable, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'RxJS';
+  obs: any;
+
+
+  ngOnInit() {
+    this.obs = Observable.create(function (observer: any) {
+      observer.next(1);
+      observer.next(2);
+      observer.next(3);
+      setTimeout(() => {
+        observer.next(4);
+        observer.complete();
+      }, 1000);
+    }
+    );
+  }
+
+  rxJSFunction() {
+      console.log('just before subscribe');
+
+    this.obs.subscribe({
+      next: (x:any) => console.log('got value ' + x),
+      error: (err:any) => console.log('someting wrong occurred: ' + err),
+      complete: () => console.log('done'),
+    });
+    console.log('just after subscribe');
+  }
+
 }
